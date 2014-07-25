6d2db882a743c901b947dfe4338d7ab6e8f3e9ea
Andrew Mao <mizzao@gmail.com>
2014-07-25 14:56:37 -0400

    Make sure update command also initializes new repos

--- git-update-submodule.sh
+++ git-update-submodule.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This should only be used on the deployment server, as it resets the tracking branches of the submodules.
-git submodule update --recursive
+git submodule update --init --recursive
